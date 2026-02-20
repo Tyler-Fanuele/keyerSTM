656da9db77687a687d88474148df7fefa76e7c63
Tyler Fanuele <tfanuele@gmail.com>
2026-02-20 10:50:17 -0500

    Modified pinnouts to make room for LCD

--- include/pinsLocations.h
+++ include/pinsLocations.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define BUZZER_PIN (GPIO_PIN_6)
+#define BUZZER_PIN (GPIO_PIN_0)
 #define BUZZER_PIN_PORT (GPIOB)
 
-#define TIP_PIN (GPIO_PIN_1)
+#define TIP_PIN (GPIO_PIN_3)
 #define TIP_PIN_PORT (GPIOB)
 
-#define RING1_PIN (GPIO_PIN_8)
-#define RING1_PIN_PORT (GPIOA)
+#define RING1_PIN (GPIO_PIN_4)
+#define RING1_PIN_PORT (GPIOB)
